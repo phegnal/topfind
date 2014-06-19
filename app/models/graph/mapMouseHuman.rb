3c823af864b886f06895c35e9052914f8ec935f3
Nik <Nik@Nikolauss-MacBook-Pro.local>
2014-06-19 12:26:17 -0700

    absolute path updated to relative path in mapMouseHuman.rb Nik

--- app/models/graph/mapMouseHuman.rb
+++ app/models/graph/mapMouseHuman.rb
@@ -4,7 +4,7 @@ class MapMouseHuman
   
   def initialize()
     @map = []
-    File.open("/Users/Nik/Desktop/PhD/dry_lab/R/Network_Files/merged/one2one_large.csv").readlines.each do |line|
+    File.open("databases/one2one_large.csv").readlines.each do |line|
        l = line.split(";")
        @map << {"h" => l[1].strip(), "m" => l[0].strip()}
     end
